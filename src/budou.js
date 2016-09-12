616a3899c0eddc55f67d228f0de990a994e7195e
Teruki Bito <bito@gocco.co.jp>
2016-09-12 15:27:13 +0900

    update: 助詞を結合するようにした

--- src/budou.js
+++ src/budou.js
@@ -1,6 +1,8 @@
+
+const re = new RegExp(/[一-龠々〆ヵヶ]+|[ぁ-ん]+|[ァ-ヴー]+|[a-zA-Z0-9]+|[ａ-ｚＡ-Ｚ０-９]+|[,.、。！!？?()（）「」『』 　]+/g);
+const joshi = new RegExp(/(でなければ|について|ならば|までを|までの|くらい|なのか|として|とは|なら|から|まで|して|だけ|より|ほど|など|って|では|は|で|を|の|が|に|へ|と|て)/g);
+
 function SimpleAnalyze(str = '') {
-  const re = new RegExp(/[一-龠々〆ヵヶ]+|[ぁ-ん]+|[ァ-ヴー]+|[a-zA-Z0-9]+|[ａ-ｚＡ-Ｚ０-９]+|[,.、。！!？?()（）「」『』 　]+/g);
-  const joshi = new RegExp(/(でなければ|について|ならば|までを|までの|くらい|なのか|として|とは|なら|から|まで|して|だけ|より|ほど|など|って|では|は|で|を|の|が|に|へ|と|て)/g);
   const s = str.replace(joshi, "$1|");
   const ary = s.split("|");
   let result = [];
@@ -26,7 +28,7 @@ export default function Budou(text = '', userOption = {}) {
   const tokens = SimpleAnalyze(text);
   const words = [];
   tokens.forEach((word) => {
-    if (word.match(/[,.、。！!？?()（）「」『』 　]+/)) {
+    if (word.match(/[,.、。！!？?()（）「」『』 　]+/) || word.match(joshi)) {
       words[words.length - 1] += word;
     } else {
       words.push(word);
