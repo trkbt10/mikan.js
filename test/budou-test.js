f57353ae2e2b66a75bb3d0d4f736142b1feebe4e
Teruki Bito <bito@gocco.co.jp>
2016-09-14 14:10:40 +0900

    update: テストの更新

--- test/budou-test.js
+++ test/budou-test.js
@@ -1,19 +1,14 @@
 import Budou from '../src/budou.js';
+const {ok, equal} = require('assert');
 
-const case1 = '常に最新、最高のモバイル。Androidを開発した同じチームから。';
-console.log(Budou(case1));
+const case1 = Budou('我輩は猫である。名前はまだない。');
+ok(case1.indexOf('<span style="display:inline-block">') > -1);
 
-const case2 = '我輩は猫である。名前はまだない。';
-console.log(Budou(case2));
+const case2 = Budou('私は好きにした。君たちも好きにしろ。', { className : 'wbr', style : '' });
+ok(case2.indexOf('class="wbr"') > -1 && case2.indexOf('style=') <= -1);
 
-const case3 = `あゝいゝな　せいせいするな
-風が吹くし
-農具はぴかぴか光つてゐるし
-山はぼんやり
-岩頸だつて岩鐘だつて
-みんな時間のないころのゆめをみてゐるのだ`;
-console.log(Budou(case3));
+const case3 = Budou('え、蒲田に！？', { className : '', style : '' });
+ok(case3.indexOf('class=') <= -1 && case3.indexOf('style=') <= -1);
 
-const text = Budou('常に最新、最高のモバイル。Androidを開発した同じチームから。');
-
-console.log(text);
+const case4 = Budou('原稿と防災服を用意してくれ', { className : '', style : 'font-weight:bold' });
+ok(case4.indexOf('style="font-weight:bold"') > -1);
