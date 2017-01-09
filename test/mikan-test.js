d2f6d2ad8fd5426aa2b3b3f042a9beee3efeb362
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-09 18:35:19 +0900

    add role='presentation'

--- test/mikan-test.js
+++ test/mikan-test.js
@@ -2,7 +2,7 @@ const Mikan = require('../src/mikan.js');
 const {ok, equal} = require('assert');
 
 const case1 = Mikan('我輩は猫である。名前はまだない。');
-ok(case1.indexOf('<span style="display:inline-block">') > -1);
+ok(case1.indexOf('<span style="display:inline-block" role="presentation">') > -1);
 
 const case2 = Mikan('私は好きにした。君たちも好きにしろ。', { className : 'wbr', style : '' });
 ok(case2.indexOf('class="wbr"') > -1 && case2.indexOf('style=') <= -1);
