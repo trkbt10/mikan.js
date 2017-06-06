5bb2651287e86687492d8721b983e075b5dcf29d
dino <dino@hyper-unko.local>
2017-06-06 11:45:36 +0900

    Add single-byte space support

--- src/mikan.js
+++ src/mikan.js
@@ -1,5 +1,5 @@
 "use strict";
-(function (root, factory) {
+(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD
     define([], factory);
@@ -10,46 +10,90 @@
     // Browser globals
     root.Mikan = factory();
   }
-})(this, function () {
+})(this, function() {
 
-  var joshi = /^(でなければ|について|ならば|までを|までの|くらい|なのか|として|とは|なら|から|まで|して|だけ|より|ほど|など|って|では|は|で|を|の|が|に|へ|と|て|じ)$/g;
-  var keywords = /([\(（「『]+.*?[\)）」』]|[a-zA-Z0-9]+\.[a-z]{2,}|[一-龠々〆ヵヶゝ]+|[ぁ-んゝ]+|[ァ-ヴー]+|[a-zA-Z0-9]+|[ａ-ｚＡ-Ｚ０-９]+)/g;
-  var periods  = /([\.\,。、！\!？\?]+)$/
+  var joshi = /(でなければ|について|かしら|くらい|けれど|なのか|ばかり|ながら|ことよ|こそ|こと|さえ|しか|した|たり|だけ|だに|だの|つつ|ても|てよ|でも|とも|から|など|なり|ので|のに|ほど|まで|もの|やら|より|って|で|と|な|に|ね|の|も|は|ば|へ|や|わ|を|か|が|さ|し|ぞ|て)/g;
+  var keywords = /(\&nbsp;|[a-zA-Z0-9]+\.[a-z]{2,}|[一-龠々〆ヵヶゝ]+|[ぁ-んゝ]+|[ァ-ヴー]+|[a-zA-Z0-9]+|[ａ-ｚＡ-Ｚ０-９]+)/g;
+  var periods = /([\.\,。、！\!？\?]+)$/g
+  var bracketsBegin = /([\(（「『])/g
+  var bracketsEnd = /([\)）」』])/g
 
   function SimpleAnalyze(str) {
-    var words = str.split(keywords).filter(function(word) {
+    var words = str.split(keywords).reduce(function(prev, word) {
+      return [].concat(prev, word.split(joshi));
+    }).reduce(function(prev, word) {
+      return [].concat(prev, word.split(bracketsBegin));
+    }).reduce(function(prev, word) {
+      return [].concat(prev, word.split(bracketsEnd));
+    }).filter(function(word) {
       return word;
     });
 
     var result = [];
-    var prevWordType = '';
+    var prevType = '';
+    var prevWord = '';
     words.forEach(function(word) {
-      var token = word.match(joshi) || word.match(periods);
+      var token = word.match(periods) || word.match(joshi)
+
+      if (word.match(bracketsBegin)) {
+        prevType = 'bracketBegin';
+        prevWord = word;
+        return
+      }
+      if (word.match(bracketsEnd)) {
+        result[result.length - 1] += word;
+        prevType = 'bracketEnd';
+        prevWord = word;
+        return
+      }
+
+      if (prevType === 'bracketBegin') {
+        word = prevWord + word
+        prevWord = ''
+        prevType = ''
+      }
+
+      // すでに文字が入っている上で助詞が続く場合は結合する
+      if (result.length > 0 && token && prevType === '') {
+        result[result.length - 1] += word;
+        prevType = 'keyword';
+        prevWord = word;
+        return
+      }
+
       // 単語のあとの文字がひらがななら結合する
-      if (token || (prevWordType === 'keyword' && word.match(/[ぁ-んゝ]+/g))) {
-        result[result.length - 1] += (token instanceof Array) ? token.shift() : word;
-        prevWordType = '';
+      if (token || (prevType === 'keyword' && word.match(/[ぁ-んゝ]+/g))) {
+        result[result.length - 1] += word;
+        prevType = ''
+        prevWord = word;
         return;
       }
-      prevWordType = 'keyword';
       result.push(word);
+      prevType = 'keyword';
+      prevWord = word;
     });
 
     return result;
   }
 
   function Mikan() {
-    var text = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
-    var userOption = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+    var text = arguments.length <= 0 || arguments[0] === undefined
+      ? ''
+      : arguments[0];
+    var userOption = arguments.length <= 1 || arguments[1] === undefined
+      ? {}
+      : arguments[1];
 
     var defaultOption = {
-      style : 'display:inline-block',
+      style: 'display:inline-block',
       role: 'presentation',
-      className : ''
+      className: ''
     };
     var option = {};
     Object.keys(defaultOption).forEach(function(key) {
-      option[key] = (typeof userOption[key] === 'undefined') ? defaultOption[key] : userOption[key];
+      option[key] = (typeof userOption[key] === 'undefined')
+        ? defaultOption[key]
+        : userOption[key];
     });
 
     var attr = '';
@@ -60,7 +104,7 @@
     var words = SimpleAnalyze(text);
 
     var html = words.map(function(word) {
-      return '<span' + attr +'>' + word + '</span>'
+      return '<span' + attr + '>' + word + '</span>'
     }).join('');
     return html;
   }
--- test/mikan-test.js
+++ test/mikan-test.js
@@ -1,36 +1,63 @@
-const Mikan = require('../src/mikan.js');
-const {ok, equal} = require('assert');
-
-const case1 = Mikan('常に最新、最高のモバイル。Androidを開発した同じチームから。');
-const case1Split = Mikan.split('常に最新、最高のモバイル。Androidを開発した同じチームから。');
-const case1result = ['常に', '最新、', '最高の', 'モバイル。', 'Androidを', '開発した', '同じ', 'チームから。'];
-ok(case1.indexOf('<span style="display:inline-block" role="presentation">') > -1);
-ok(tagToArray(case1).every((txt, i) => txt === case1result[i]));
-ok(case1Split.every((txt, i) => txt === case1result[i]));
-
-const case2 = Mikan('私は好きにした。君たちも好きにしろ。', { className : 'wbr', style : '' });
-ok(case2.indexOf('class="wbr"') > -1 && case2.indexOf('style=') <= -1);
-
-const case3 = Mikan('え、蒲田に！？', { className : '', style : '' });
-ok(case3.indexOf('class=') <= -1 && case3.indexOf('style=') <= -1);
-
-const case4 = Mikan('原稿と防災服を用意してくれ', { className : '', style : 'font-weight:bold' });
-const case4result = [ '原稿と', '防災服を', '用意してくれ' ];
-ok(case4.indexOf('style="font-weight:bold"') > -1);
-ok(tagToArray(case4).every((txt, i) => txt === case4result[i]));
-
-const case5 = Mikan('このmikan.jsというライブラリは、スマートな文字区切りを可能にします。', { className : '', style : '', role: "" });
-const case5result = [ 'この', 'mikan.jsという', 'ライブラリは、', 'スマートな', '文字区切りを', '可能にします。' ];
-ok(tagToArray(case5).every((txt, i) => txt === case5result[i]));
-
-const case6 = Mikan('「あれ」でもない、「これ」でもない。');
-const case6Split = Mikan.split('「あれ」でもない、「これ」でもない。');
-const case6result = ['「あれ」でもない、', '「これ」でもない。'];
-ok(case6.indexOf('<span style="display:inline-block" role="presentation">') > -1);
-ok(tagToArray(case6).every((txt, i) => txt === case6result[i]));
-ok(case6Split.every((txt, i) => txt === case6result[i]));
-
+const mikan = require('../src/mikan.js')
+const test = require('ava')
 
 function tagToArray(text = "") {
-  return text.split(/<.*?>(.*?)<\/.*?>/g).filter((word) => word);
+  return text.split(/<.*?>(.*?)<\/.*?>/g).filter((word) => word)
 }
+
+
+test(t => {
+  const source = '常に最新、最高のモバイル。Androidを開発した同じチームから。'
+  const expected = ['常に', '最新、', '最高の', 'モバイル。', 'Androidを', '開発した', '同じ', 'チームから。']
+
+  const queue = mikan.split(source)
+  const result = mikan(source)
+
+  t.deepEqual(queue, expected)
+  t.deepEqual(tagToArray(result), expected)
+  t.truthy(result.indexOf('<span style="display:inline-block" role="presentation">') > -1)
+})
+
+test(t => {
+  const source = '私は好きにした。君たちも好きにしろ。'
+  const result = mikan(source, { className : 'wbr', style : 'font-weight:bold', role : 'debag' })
+  t.truthy(result.indexOf('class="wbr"') > -1)
+  t.truthy(result.indexOf('style="font-weight:bold"') > -1)
+  t.truthy(result.indexOf('role="debag"') > -1)
+})
+
+test(t => {
+  const source = 'え、蒲田に！？'
+  const result = mikan(source, { className : '', style : '', role : '' })
+  t.truthy(result.indexOf('class=') <= -1)
+  t.truthy(result.indexOf('style=') <= -1)
+  t.truthy(result.indexOf('role=') <= -1)
+})
+
+test(t => {
+  const source = '原稿と防災服を用意してくれ'
+  const expected = [ '原稿と', '防災服を', '用意してくれ' ]
+  const result = mikan.split(source)
+  t.deepEqual(expected, expected)
+})
+
+test(t => {
+  const source = 'このmikan.jsというライブラリは、スマートな文字区切りを可能にします。'
+  const expected = [ 'この', 'mikan.jsと', 'いう', 'ライブラリは、', 'スマートな', '文字区切りを', '可能にします。' ]
+  const result = mikan.split(source)
+  t.deepEqual(result, expected)
+})
+
+test(t => {
+  const source = '「あれ」でもない、「これ」でもない。'
+  const expected =  ['「あれ」', 'でもない、', '「これ」', 'でもない。']
+  const result = mikan.split(source)
+  t.deepEqual(result, expected)
+})
+
+test(t => {
+  const source = '半角スペース 対応'
+  const expected = [ '半角', 'スペース', ' ', '対応' ]
+  const result = mikan.split(source)
+  t.deepEqual(result, expected)
+})
